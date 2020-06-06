8bf341b9c2e2071201ced59bd3cd58a07b4fe83f
Guillermo Felipetti <guill388@hotmail.com>
2020-06-06 12:51:30 -0300

    1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,9 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+data Gimnasta = Gimnasta String Float Float Float deriving(Show)
+
+pancho = Gimnasta "Francisco" 40.0 120.0 1.0
+
+andres = Gimnasta "Andy" 22.0 80.0 6.0
